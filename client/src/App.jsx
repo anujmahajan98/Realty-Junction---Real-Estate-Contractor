e08f31fd5404523244b6cc4aac9372b7fffb2211
Abhishek <abhishek75singh99@gmail.com>
2023-12-01 07:04:42 +0530

    fix About page name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import About from "./pages/about";
+import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/Signin";
